af3ba66eb6267879205c39148d57c373e72f0a5d
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-03-21 11:20:26 +0530

    Create ajenti.sh

---
+++ ajenti.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+lsb_release -a
+if [[ $(lsb_release -rs) == "16.04" ]]; then # replace 8.04 by the number of release you want
+
+      echo "Compatible version"
+      apt-get update 
+      apt-get install nginx fail2ban -y 
+      wget http://repo.ajenti.org/debian/key -O- | apt-key add - 
+      echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" >> /etc/apt/sources.list 
+      apt-get update 
+      apt-get install ajenti -y 
+      service ajenti restart 
+      apt install python pip -y 
+      pip install --upgrade pip pip install speedtest-cli 
+      speedtest-cli
+      sudo add-apt-repository ppa:certbot/certbot 
+      sudo apt-get update 
+      sudo apt-get install python-certbot-nginx -y 
+      sudo systemctl enable nginx 
+      sudo systemctl enable ajenti 
+      sudo systemctl status nginx 
+      sudo systemctl status ajenti 
+      sudo systemctl restart ajenti 
+      sudo systemctl restart nginx
+else
+      echo "Non-compatible ubuntu version"
+fi
