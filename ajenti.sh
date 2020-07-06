09b51984674cc167e8a0cf03b1f4e5027af47dd9
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-07-06 12:57:40 +0530

    Update ajenti.sh

--- ajenti.sh
+++ ajenti.sh
@@ -1,3 +1,4 @@
+
 #Ajenti
 sudo apt-get update && sudo apt-get install nginx fail2ban -y && wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
 
@@ -6,4 +7,4 @@ sudo apt-get update && sudo apt-get install nginx fail2ban -y && wget http://rep
 echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" >> sudo /etc/apt/sources.list 
 # setup shit and speed test. 
 
-sudo service ajenti restart && sudo apt install python pip -y && sudo pip install --upgrade pip && sudo pip install speedtest-cli && sudo add-apt-repository ppa:certbot/certbot && sudo systemctl enable nginx && sudo systemctl enable ajenti && sudo systemctl status nginx && sudo systemctl status ajenti && sudo systemctl restart ajenti && sudo systemctl restart nginx && speedtest-cli
+sudo service ajenti restart && sudo apt-get update && sudo apt-get install python-pip -y && sudo pip install --upgrade pip && sudo pip install speedtest-cli && sudo add-apt-repository ppa:certbot/certbot && sudo systemctl enable nginx && sudo systemctl enable ajenti && sudo systemctl status nginx && sudo systemctl status ajenti && sudo systemctl restart ajenti && sudo systemctl restart nginx && speedtest-cli
