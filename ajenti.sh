11c24d60c231a88f9dfb9afb0e3b318149c02130
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-08-27 22:48:31 +0530

    Update ajenti.sh

--- ajenti.sh
+++ ajenti.sh
@@ -8,4 +8,4 @@ echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" >> sudo /etc/apt/so
 
 
 # setup shit and speed test. 
-sudo apt-get update && apt-get install ajenti -y && sudo service ajenti restart && sudo apt-get update && sudo apt-get install python-pip -y && sudo pip install --upgrade pip && sudo pip install speedtest-cli && sudo add-apt-repository ppa:certbot/certbot && sudo systemctl enable nginx && sudo systemctl enable ajenti && sudo systemctl status nginx && sudo systemctl status ajenti && sudo systemctl restart ajenti && sudo systemctl restart nginx && speedtest-cli
+sudo apt-get update && apt-get install ajenti -y && sudo service ajenti restart && sudo apt-get update && sudo apt-get install python-pip -y && sudo pip install --upgrade pip && sudo pip install speedtest-cli && sudo systemctl enable nginx && sudo systemctl enable ajenti && sudo systemctl status nginx && sudo systemctl status ajenti && sudo systemctl restart ajenti && sudo systemctl restart nginx && speedtest-cli
