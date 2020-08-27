f8292322c8b9e81f1844a17c87007caa9c7b4c66
Sawada Tsunayoshi <34431649+TsunayoshiSawada@users.noreply.github.com>
2020-08-27 22:54:14 +0530

    Update ajenti.sh

--- ajenti.sh
+++ ajenti.sh
@@ -2,10 +2,8 @@
 #Ajenti
 sudo apt-get update && sudo apt-get install nginx fail2ban -y && wget http://repo.ajenti.org/debian/key -O- | sudo apt-key add -
 sudo apt install nano
-
-
-echo "deb http://repo.ajenti.org/ng/debian main main ubuntu" >> sudo /etc/apt/sources.list && cd sudo /etc/apt/ && nano sources.list
-
+cd /etc/apt/ && nano sources.list
+sudo sh -c 'echo deb http://repo.ajenti.org/ng/debian main main ubuntu >> sudo /etc/apt/sources.list'
 
 # setup shit and speed test. 
 sudo apt-get update && apt-get install ajenti -y && sudo service ajenti restart && sudo apt-get update && sudo apt-get install python-pip -y && sudo pip install --upgrade pip && sudo pip install speedtest-cli && sudo systemctl enable nginx && sudo systemctl enable ajenti && sudo systemctl status nginx && sudo systemctl status ajenti && sudo systemctl restart ajenti && sudo systemctl restart nginx && speedtest-cli
